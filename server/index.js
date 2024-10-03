2aa615ac6967502c124dbc33bac4aa1338f79408
Alex <alexpleonhardt@gmail.com>
2024-10-03 17:58:31 -0400

    serving html chatbox

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import { createServer  } from 'http';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const PORT = 3001;
 
 const app = express();
 const server = createServer(app);
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
 server.listen(PORT, () => {
